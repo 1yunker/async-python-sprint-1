ddd2c281388d16299f0111dc5fe12d1e51267c62
Yuri Pomazkin <1yunker@gmail.com>
2023-10-31 01:05:35 +0500

    Initial commit

--- forecasting.py
+++ forecasting.py
@@ -1,29 +1,79 @@
-# import logging
-# import threading
 # import subprocess
-# import multiprocessing
+from itertools import count
+import json
+import multiprocessing
+import numpy
+# import shutil
 
-
-from external.client import YandexWeatherAPI
+# from concurrent.futures import ThreadPoolExecutor
+from pandas import DataFrame
+from threading import Thread
 from tasks import (
-    DataFetchingTask,
-    DataCalculationTask,
-    DataAggregationTask,
+    # DataAggregationTask,
     DataAnalyzingTask,
+    DataCalculationTask,
+    DataFetchingTask,
 )
-from utils import CITIES, get_url_by_city_name
+from utils import CITIES
 
 
 def forecast_weather():
     """
-    Анализ погодных условий по городам
+    Анализ погодных условий по городам.
     """
-    # city_name = "MOSCOW"
-    # url_with_data = get_url_by_city_name(city_name)
-    # resp = YandexWeatherAPI.get_forecasting(url_with_data)
-    # print(resp)
-    pass
+
+    # Создаем очередь для городов с валидными данными
+    queue = multiprocessing.Manager().Queue()
+    fetching_task = DataFetchingTask(queue)
+    calculation_task = DataCalculationTask(queue)
+
+    processing_thread = Thread(target=calculation_task.process_queue)
+    processing_thread.start()
+
+    with multiprocessing.Pool() as pool:
+        pool.map(fetching_task.get_and_put_data, CITIES.keys())
+
+    queue.put(None)
+    processing_thread.join()
+
+    # Получаем города, по которым успешно прошло вычисление погодных параметров
+    # valid_cities = calculation_task.cities
+    # print(list(valid_cities))
+
+    columns = []
+    columns.append('City')
+    columns.append('Condition')
+    columns.extend(calculation_task.dates)
+    columns.append('Avg')
+    # columns.append('Rating')
+
+    df = DataFrame(columns=columns)
+    for city_name in calculation_task.cities:
+        with open("results/{}_output.json".format(city_name), "r") as file:
+            data_read = json.load(file).get('days')
+            lst_temps = []
+            lst_hours = []
+            for item in data_read:
+                lst_temps.append(item.get('temp_avg'))
+                lst_hours.append(item.get('relevant_cond_hours'))
+
+            lst = [city_name, 'temp_avg']
+            lst.extend(lst_temps)
+            lst.append(numpy.average([d for d in lst_temps if d is not None]))
+            df.loc[len(df)] = lst
+            lst = [city_name, 'relevant_cond_hours']
+            lst.extend(lst_hours)
+            lst.append(numpy.average([d for d in lst_hours if d is not None]))
+            df.loc[len(df)] = lst
+
+    df.to_json('results/aggregation.json', orient='records', indent=4)
+
+    # file_with_path = '/results/aggregation.csv'
+    # DataAggregationTask.aggregate_data_to_csv(valid_cities, file_with_path)
+    # DataAnalyzingTask.make_result_from(file_with_path)
+
+    # shutil.rmtree('results')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     forecast_weather()
--- tasks.py
+++ tasks.py
@@ -1,14 +1,124 @@
+# import csv
+import json
+import logging
+import os
+from multiprocessing import Queue
+# from pandas import DataFrame
+
+from external.analyzer import (
+    load_data,
+    analyze_json,
+    dump_data
+)
+from external.client import YandexWeatherAPI
+from utils import get_url_by_city_name
+
+
+format = '%(asctime)s [%(levelname)s]: %(message)s'
+logging.basicConfig(
+    filename='forecasting.log',
+    encoding='utf-8',
+    format=format,
+    level=logging.INFO
+)
+logger = logging.getLogger()
+
+
 class DataFetchingTask:
-    pass
+    """
+    Получение данных через API и их запись в JSON-файл.
+    """
+    def __init__(self, queue: Queue):
+        self.queue = queue
+
+    def get_and_put_data(self, city_name: str) -> None:
+        try:
+            url_with_data = get_url_by_city_name(city_name)
+            resp = YandexWeatherAPI.get_forecasting(url_with_data)
+            if resp:
+                file_with_path = "results/{}_response.json".format(city_name)
+                with open(file_with_path, "w") as file:
+                    json.dump(resp, file, indent=4)
+                    logger.info(
+                        f'Информация о городе {city_name} успешно записана.'
+                    )
+
+                self.queue.put(city_name)
+
+        except Exception as error_msg:
+            logger.error(
+                f'При получении данных о городе {city_name} возникла ошибка: '
+                f'{error_msg}.'
+            )
 
 
 class DataCalculationTask:
-    pass
+    """
+    Вычисление погодных параметров и их запись в JSON-файл.
+    """
+
+    def __init__(self, queue: Queue):
+        self.queue = queue
+        self.cities = []
+        self.dates = []
+
+    def process_queue(self) -> None:
+        while True:
+            city_name = self.queue.get()
+            if city_name is None:
+                break
+            # arg1 = '-i results/{}_response.json'.format(city_name)
+            # arg2 = '-o results/{}_output.json'.format(city_name)
+            # command = f'python3 external/analyzer.py {arg1} {arg2}'
+            # os.system(command)
+            data = load_data(
+                input_path='results/{}_response.json'.format(city_name)
+            )
+            data = analyze_json(data)
+            dump_data(
+                data,
+                output_path='results/{}_output.json'.format(city_name)
+            )
+
+            logger.info(f'Информация о городе {city_name} успешно обработана.')
+
+            # Добавляем город в список обработанных
+            self.cities.append(city_name)
+            # Добавляем даты для анализа
+            list_of_days = data.get('days')
+            dates = [d['date'] for d in list_of_days if 'date' in d]
+            self.dates = sorted(dates)
+            # the work is done
+            self.queue.task_done()
 
 
 class DataAggregationTask:
-    pass
+    """
+    Объединение вычисленных данных.
+    """
+
+    def aggregate_data(cities, file_with_path):
+
+        logger.info(
+            f'Данные успешно агрегированы и сохранены в {file_with_path}.'
+        )
+
+    def save_data_to_csv(data, file_with_path):
+
+        logger.info(
+            f'Данные успешно агрегированы и сохранены в {file_with_path}.'
+        )
 
 
 class DataAnalyzingTask:
-    pass
+    """
+    Финальный анализ и получение результата.
+    """
+
+    def make_result_from(file_with_path):
+        logger.info(f'Данные успешно прочитаны из {file_with_path}.')
+
+        city_name = 'MOSCOW'
+        print(f'{city_name} наиболее блвгоприятен для поездки.')
+
+        logger.info('Вывод финального результата.')
